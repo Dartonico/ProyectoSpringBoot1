093330ace5ec69b67e91d08be19801914821ca65
Dartonico <123487092+Dartonico@users.noreply.github.com>
2023-03-16 10:19:50 +0100

    PROYEC-4

--- src/main/java/com/eoi/paradigmaspoo/DemoParadigmas.java
+++ src/main/java/com/eoi/paradigmaspoo/DemoParadigmas.java
@@ -27,6 +27,7 @@ public class DemoParadigmas {
         Mesa mesaCristal = new Mesa("Mesa Bonita de cristal", 90, 100, 100, 15, "Madera y Cristal","Salón", pata1, pata2, pata3, pata4, tableroCristal);
         mesaCristal.showInfo();
 
+
     }
 
 }
